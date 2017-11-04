d05b776ac80eae998f50931c3707e915bd118bd5
Tamara Temple <tamouse@gmail.com>
2017-11-04 06:13:30 -0500

    Add ingredient spec

--- rails_api/spec/models/ingredient_spec.rb
+++ rails_api/spec/models/ingredient_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  let(:user) {FactoryGirl.create(:user)}
-  let(:recipe) {user.recipes.create(FactoryGirl.attributes_for(:recipe))}
+  let(:user) { FactoryGirl.create(:user) }
+  let(:recipe) { user.recipes.create(FactoryGirl.attributes_for(:recipe)) }
 
   it "should create an ingredient" do
-
+    ingredient = recipe.ingredients.create(
+      name:       'Apple',
+      quantity:   1,
+      measure:    Measure.find_by(name: 'whole'),
+      prep_notes: "peeled and chopped"
+    )
+    expect(ingredient.to_s).to eq("1 whole Apple, peeled and chopped")
   end
 end
